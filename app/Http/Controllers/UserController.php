cd48d0f51f4113fe6644e8933c424f59d09765d2
IDCDEV03 <idcdev03@iddrives.co.th>
2023-08-05 09:06:00 +0700

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
