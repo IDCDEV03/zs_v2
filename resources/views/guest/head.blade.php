fd98293254a9e8c7f1e31c414aa6ed14e8b1af51
IDCDEV03 <idcdev03@iddrives.co.th>
2023-08-10 11:00:20 +0700

    update

---
+++ resources/views/guest/head.blade.php
@@ -0,0 +1,40 @@
+<header class="header-top">
+    <nav class="navbar navbar-light">
+       <div class="navbar-left">
+          <div class="logo-area">
+             <a class="navbar-brand" href="#">
+               <img src="{{ asset('theme/img/logo03.png')}}" alt="logo" height="50px"
+               width="60px">
+             </a>
+      
+          </div>
+       </div>
+             
+       <!-- ends: navbar-left -->
+       <div class="navbar-right">
+          <ul class="navbar-right__menu">
+             <li class="nav-search">
+                <a href="#" class="search-toggle">
+                   <i class="uil uil-search"></i>
+                   <i class="uil uil-times"></i>
+                </a>
+                <form action="/" class="search-form-topMenu">
+                   <span class="search-icon uil uil-search"></span>
+                   <input class="form-control me-sm-2 box-shadow-none" type="search" placeholder="Search..." aria-label="Search">
+                </form>
+             </li>
+     
+             <!-- ends: .nav-flag-select -->
+           
+            
+          </ul>
+          <!-- ends: .navbar-right__menu -->
+          <div class="navbar-right__mobileAction d-md-none">
+         
+             <a href="#" class="btn-author-action">
+                <img class="svg" src="{{ asset('theme/img/svg/more-vertical.svg')}}" alt="more-vertical"></a>
+          </div>
+       </div>
+       <!-- ends: .navbar-right -->
+    </nav>
+ </header>
---
+++ resources/views/guest/header.blade.php
@@ -0,0 +1,44 @@
+<!-- inject:css-->
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/bootstrap/bootstrap.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/daterangepicker.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/fontawesome.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/footable.standalone.min.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/fullcalendar@5.2.0.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/jquery-jvectormap-2.0.5.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/jquery.mCustomScrollbar.min.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/leaflet.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/line-awesome.min.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/magnific-popup.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/MarkerCluster.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/MarkerCluster.Default.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/select2.min.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/slick.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/star-rating-svg.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/trumbowyg.min.css') }}">
+
+<link rel="stylesheet" href="{{ asset('theme/assets/vendor_assets/css/wickedpicker.min.css') }}">
+
+<link rel="stylesheet" href="{{ asset('style.css') }}">
+
+<!-- endinject -->
+
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('theme/img/logo_fav.png')}}">
+
+<!-- Fonts -->
+<link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
